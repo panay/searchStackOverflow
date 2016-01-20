f730b86724ab6b0a01e540a4eb15d61dd37acfca
Sergey Panay <sergey.panay@nutnet.ru>
2016-01-20 17:44:43 +0400

    initial commit

--- app/controllers/AnswersController.js
+++ app/controllers/AnswersController.js
@@ -2,10 +2,29 @@ var searchApp = angular.module('searchApp');
 
 searchApp.controller('AnswersController', function ($scope, $http, $routeParams) {
 
+  var body = angular.element(document.querySelector('body'));
+
+  $scope.loading = true;
+
   $http.get(
-      'https://api.stackexchange.com/2.2/questions/' + $routeParams.answer_id + '/answers?order=desc&sort=activity&site=stackoverflow&filter=!b0OfNb3HQOAMVH'
+      'https://api.stackexchange.com/2.2/questions/' + $routeParams.answer_id + '?order=desc&sort=activity&site=stackoverflow&filter=!-*f(6rc.(Xr5'
   ).success(function (json) {
 
-    $scope.answers = json.items;
+    $scope.loading = false;
+
+    var question = json.items[0];
+
+    $scope.question = {
+      title: question.title,
+      body: question.body,
+      author: question.owner.display_name,
+      answers: question.answers
+    };
+
+    body.removeClass('show-panel');
+  }).error(function(json) {
+
+    $scope.loading = false;
+    $scope.errorMessage = json.error_message;
   });
 });
--- app/controllers/SearchFormController.js
+++ app/controllers/SearchFormController.js
@@ -4,13 +4,21 @@ searchApp.controller('SearchFormController', function ($scope, $http, $location)
 
   $scope.getResult = function (search, searchForm) {
 
-    $scope.errorMessage = '';
+    $scope.loading = false;
 
     if (searchForm.$valid) {
+      $scope.loading = true;
+
       $http.get(
           'https://api.stackexchange.com/2.2/search/advanced?order=asc&sort=activity&title=' + search.input + '&site=stackoverflow'
       ).success(function (json) {
+
         $location.path('/search-result').search({'query': search.input, 'result': JSON.stringify(json.items)});
+        $scope.loading = false;
+      }).error(function (json) {
+
+        $scope.errorMessage = json.error_message;
+        $scope.loading = false;
       });
     }
   };
